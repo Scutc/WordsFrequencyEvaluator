f2bec077891fd01ca7b57df65608b582948fbdc2
Крючков Дмитрий Игоревич <d.i.kryuchkov@tinkoff.ru>
2023-12-21 16:12:58 +0300

    Initial commit

---
+++ src/main/java/com/cyolo/WordsFrequencyEvaluator.java
@@ -0,0 +1,10 @@
+package com.cyolo;
+
+import io.micronaut.runtime.Micronaut;
+
+public class WordsFrequencyEvaluator {
+
+    public static void main(String[] args) {
+        Micronaut.run(WordsFrequencyEvaluator.class, args);
+    }
+}
---
+++ src/main/java/com/cyolo/controller/WordController.java
@@ -0,0 +1,17 @@
+package com.cyolo.controller;
+
+import io.micronaut.http.annotation.Controller;
+
+@Controller
+public class WordController implements WordsApi {
+
+    @Override
+    public void addWords(String body) {
+
+    }
+
+    @Override
+    public void getWords() {
+
+    }
+}
